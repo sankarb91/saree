84d282e3bd67d960bf2f89cbe17e83285c1a8c4c
sankarb91 <shankarb91@gmail.com>
2023-07-24 06:28:33 +0000

    Adding new feature for saree section

---
+++ src/main/java/com/saravanastore/saree/App.java
@@ -0,0 +1,13 @@
+package com.saravanastore.saree;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
